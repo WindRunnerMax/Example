b8ce4bd568756c873a032e18d60700afe0f5d379
Czy <651525974@qq.com>
2020-07-01 22:48:01 +0800

    docsify slidebar

---
+++ Docsify/catalog/index.js
@@ -0,0 +1,69 @@
+function loadCatalog(hook, vm) {
+    hook.doneEach(function() {
+        var level = [0, 0, 0];
+        var nodeList = document.getElementById("main").childNodes;
+        var catalog = document.getElementById("catalog");
+        var ul = catalog || document.createElement("ul");
+        ul.innerHTML = "";
+
+        var title = document.createElement("div");
+        title.setAttribute("id", "catalogTitle");
+        var titleText = document.createElement("div");
+        titleText.innerText = "Catalog";
+        var titleShrink = document.createElement("div");
+        titleShrink.className = "iconfont icon-youjiantou-copy";
+        title.appendChild(titleText);
+        title.appendChild(titleShrink);
+        title.addEventListener('click', (e) => {
+            ul.classList.add("hide");
+            document.getElementById("contract").classList.remove("hide");
+        })
+
+        ul.appendChild(title);
+        ul.setAttribute("id", "catalog");
+        ul.className = "catalog hide";
+        Array.prototype.forEach.call(nodeList, (v) => {
+            if (v.nodeName.match(/H[123]/)) {
+                var li = document.createElement("li");
+                if (v.nodeName === "H1") {
+                    ++level[0];
+                    level[1] = 0;
+                    level[2] = 0;
+                    // li.className = "level1";
+                    // li.innerText = `${level[0]} ${v.innerText}`;
+                } else if (v.nodeName === "H2") {
+                    ++level[1];
+                    level[2] = 0;
+                    li.className = "level2";
+                    li.innerText = `${level[0]}.${level[1]} ${v.innerText}`;
+                } else if (v.nodeName === "H3") {
+                    ++level[2];
+                    li.className = "level3";
+                    li.innerText = `${level[0]}.${level[1]}.${level[2]} ${v.innerText}`;
+                }
+                li.addEventListener('click', (e) => {
+                    window.scrollTo({ top: v.offsetTop, left: 0, behavior: 'smooth' });
+                })
+                ul.appendChild(li);
+            }
+        });
+
+        var contract = document.getElementById("contract") || document.createElement("div");
+        contract.innerHTML = "";
+        var icon = document.createElement("div");
+        icon.className = "iconfont icon-fanhui";
+        contract.appendChild(icon);
+        contract.setAttribute("id", "contract");
+        contract.className = "contract";
+        contract.addEventListener('click', (e) => {
+            ul.classList.remove("hide");
+            contract.classList.add("hide");
+        })
+
+        document.getElementsByTagName("body")[0].appendChild(ul);
+        document.getElementsByTagName("body")[0].appendChild(contract);
+    });
+}
+
+window.$docsify = window.$docsify || {};
+window.$docsify.plugins = [loadCatalog].concat(window.$docsify.plugins || []);
---
+++ Docsify/sidebar/index.js
@@ -0,0 +1,30 @@
+var loadSlidebar = function(hook, vm) {
+    hook.init(function() {
+        vm.record = [];
+    });
+
+    hook.doneEach(function() {
+        var caption = document.querySelectorAll(".sidebar-nav > ul > li > p");
+        Array.prototype.forEach.call(caption, (p, pIndex) => {
+            var ul = p.nextSibling;
+            var lis = ul.childNodes;
+            p.innerHTML = `<div>${p.innerText}</div><div class="count">${lis.length}</div>`;
+            var height = Array.prototype.reduce.call(lis, (pre, now) => pre + now.offsetHeight + 6, 0);
+            ul.classList.add("hide");
+            if(vm.record[pIndex]) ul.style.height = `${height}px`;
+            p.addEventListener("click", e => {
+                if (ul.offsetHeight) {
+                    vm.record[pIndex] = false;
+                    ul.style.height = `0px`;
+                }else {
+                    vm.record[pIndex] = true;
+                    ul.style.height = `${height}px`;
+                }
+            })
+        })
+    })
+
+}
+
+window.$docsify = window.$docsify || {};
+window.$docsify.plugins = [loadSlidebar].concat(window.$docsify.plugins || []);
